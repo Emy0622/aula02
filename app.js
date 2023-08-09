3e36ad95d7b05381d2132838de6236e982869696
YASMIN TARGINO DE ALEXANDRE <23111771@CFP127ALUNO.LOCAL>
2023-08-09 11:48:05 -0300

    ¨5/5¨

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const forma2 = document.getElementById('forma2')
 const forma3 = document.getElementById('forma3')
 const forma4 = document.getElementById('forma4')
 const forma5 = document.getElementById('forma5')
+const forma6 = document.getElementById('forma6')
+const forma7 = document.getElementById('forma7')
 
 function trocarCor() {
-    forma1.classList.add('verde')
-    forma1.classList.toggle('amarelo')
+    forma1.classList.add('roxo')
+    forma1.classList.toggle('rosa')
 }
 
 forma1.addEventListener('click', trocarCor)
@@ -16,6 +18,8 @@ forma2.addEventListener('click', trocarForma)
 forma3.addEventListener('click', ficarTransparente) 
 forma4.addEventListener('click', rodar) 
 forma5.addEventListener('click', aumentarTamanho) 
+forma6.addEventListener('click', seMovimentar) 
+forma7.addEventListener('click', brilhar) 
 
 function trocarForma (){
     forma2.classList.toggle('circulo')
@@ -34,3 +38,16 @@ function aumentarTamanho() {
     forma5.classList.add('normal')
     forma5.classList.toggle('grande')
 }
+
+function seMovimentar() {
+
+    forma6.classList.toggle('lado')
+}
+
+function brilhar() {
+
+    forma7.classList.toggle('brilhar')
+}
+
+
+
